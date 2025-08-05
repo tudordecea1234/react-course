90ef09e12e87419bae67e2a39ac0da4523eefd24
tudor <tudordecea23@gmail.com>
2025-08-05 15:06:18 +0300

    fetched data from backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -4,6 +4,13 @@ import checkmarkIcon from '../assets/images/icons/checkmark.png';
 import { products } from '../../starting/data/products';
 
 export function HomePage() {
+    fetch('http://localhost:3000/api/products')
+        .then((response) => {
+            return response.json()
+        }).then((data) => {
+            console.log(data);
+        })
+
     return (
         <>
             <Header />
@@ -24,14 +31,14 @@ export function HomePage() {
 
                                 <div className="product-rating-container">
                                     <img className="product-rating-stars"
-                                        src={`images/ratings/rating-${product.rating.stars*10}.png`} />
+                                        src={`images/ratings/rating-${product.rating.stars * 10}.png`} />
                                     <div className="product-rating-count link-primary">
                                         {product.rating.count}
                                     </div>
                                 </div>
 
                                 <div className="product-price">
-                                    ${(product.priceCents/100).toFixed(2)}
+                                    ${(product.priceCents / 100).toFixed(2)}
                                 </div>
 
                                 <div className="product-quantity-container">
